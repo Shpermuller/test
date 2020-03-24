16c614705d521f13ccec69426095011dc79c188c
Shpera <rshpera@gmail.com>
2020-03-24 11:14:19 +0200

    Это наш первый коммит

---
+++ main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var leftBorder = 1;
+let second = 2;
+const pi = 3.14;
+let string = "Roman23"
+console.log (string);
+
+let imageFind = {
+    name: "Roman",
+    age: 29,
+    isMarried: false
+}
+console.log (imageFind.name);
+
+//alert ("HEllo Roman!");
+
+let browsec = prompt("Are you here?");
+
+console.log(browsec);
+
